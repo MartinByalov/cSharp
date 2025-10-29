ca42a3f3ab13a5714b9bdb79179b516eadaf7c59
Martin Byalov <35262068+MartinByalov@users.noreply.github.com>
2025-10-30 00:41:39 +0200

    up()

---
+++ VATCalc/VATCalc.cs
@@ -0,0 +1,15 @@
+﻿public class VATCalculation
+{
+    public static void Main(string[] args)
+    {
+        double price = 100.00;
+        const double VAT_RATE = 0.20; // 20% ДДС
+        
+        // Формула: Цена + (Цена * ДДС ставка) или Цена * (1 + ДДС ставка)
+        double priceWithVAT = price * (1 + VAT_RATE);
+        
+        Console.WriteLine($"Цена без ДДС: {price} лв."); // 100 лв.
+        Console.WriteLine($"ДДС ставка: {VAT_RATE:P0}"); // :P0 форматира като процент
+        Console.WriteLine($"Цена с ДДС: {priceWithVAT} лв."); // 120.00 лв.
+    }
+}
